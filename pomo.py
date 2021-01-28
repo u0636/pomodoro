4661ffe312425c53e58e0c7fb1acfff3290a10e4
Name <email@example.com>
2021-01-28 23:46:21 +0300

    Update pomo.py
    Create function for the timer

--- pomo.py
+++ pomo.py
@@ -5,17 +5,22 @@ import time
 
 @click.command()
 @click.option('-p', '--pomodoros', default=4, help='Number of pomodoros.')
-# @click.option('-l', '--length', default=5, help='Pomodoro\'s length.')
-@click.option('-s', '--short-break', default=5, help='Short break.')
-@click.option('-l', '--long-break', default=15, help='Long break.')
-def main(pomodoros, short_break, long_break):
+@click.option('-s', '--short-break', default=2, help='Short break.')
+@click.option('-l', '--long-break', default=5, help='Long break.')
+@click.option('--pomodoros_length', default=3, help='Pomodoro\'s length.')
+def main(pomodoros, short_break, long_break, pomodoros_length):
     """Simple program for Pomodoro Technique."""
-    length = 5
-    with click.progressbar(range(length*2),
-                        label='Modifying user accounts') as bar:
-        for menute in bar:
-            time.sleep(1)
-    
+    for pomo in range(1, pomodoros+1):
+        pomodoros_length = pomodoros_length * 1
+        progressbar_label = f'Pomodoro {pomo}'
+        pomodoro_timer(pomodoros_length, progressbar_label)
+
+                
+def pomodoro_timer(pomodoros_length, progressbar_label):
+    with click.progressbar(range(pomodoros_length),
+                            label=progressbar_label) as bar:
+            for menute in bar:
+                time.sleep(1)
 
 if __name__ == '__main__':
     main()

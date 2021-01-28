9a714fef1cb3fb6e462ebd383955b6f4aeae7053
Name <email@example.com>
2021-01-29 00:59:12 +0300

    Add color for progress bars

--- pomo.py
+++ pomo.py
@@ -12,21 +12,28 @@ def main(pomodoros, short_break, long_break, pomodoros_length):
     """Tiny tool for Pomodoro Technique."""
     for pomo in range(1, pomodoros+1):
         length = pomodoros_length
-        progressbar_label = f'Pomodoro {pomo}'
-        pomodoro_timer(length, progressbar_label)
+        progressbar_label = f'Pomodoro {pomo}\t'
+        progressbar_color = 'bright_red'
+        pomodoro_timer(length, progressbar_label, progressbar_color)
         if pomo == pomodoros:
             length = long_break
-            progressbar_label = 'Long break'
-            pomodoro_timer(length, progressbar_label)
+            progressbar_label = 'Long break\t'
+            progressbar_color = 'bright_green'
+            pomodoro_timer(length, progressbar_label, progressbar_color)
         else:
             length = short_break
-            progressbar_label = 'Short break'
-            pomodoro_timer(length, progressbar_label)
+            progressbar_label = 'Short break\t'
+            progressbar_color = 'bright_blue'
+            pomodoro_timer(length, progressbar_label, progressbar_color)
 
-def pomodoro_timer(length, progressbar_label):
+def pomodoro_timer(length, progressbar_label, progressbar_color):
     length = length * 1
+    fill_char = click.style('#', fg=progressbar_color)
+    empty_char = click.style('-', fg='white')
     with click.progressbar(range(length),
-                            label=progressbar_label) as bar:
+                            label=progressbar_label,
+                            fill_char=fill_char,
+                            empty_char=empty_char) as bar:
             for menute in bar:
                 time.sleep(1)
 
